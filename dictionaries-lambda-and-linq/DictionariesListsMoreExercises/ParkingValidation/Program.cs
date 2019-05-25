dc2ff875a0ea4ad835ab8ff7edca3a3cc77514a1
star-lord <alekrifai97@gmail.com>
2019-05-25 20:16:11 +0300

    Associative Arrays, Lambda Functions and LINQ

---
+++ dictionaries-lambda-and-linq/DictionariesListsMoreExercises/ParkingValidation/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ParkingValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var parking = new Dictionary<string, string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split(' ').ToArray();
+                var command = input[0];
+                var username = input[1];
+
+                if (command == "unregister")
+                {
+                    if (!parking.ContainsKey(username))
+                    {
+                        Console.WriteLine($"ERROR: user {username} not found");
+                    }
+                    else
+                    {
+                        parking.Remove(username);
+                        Console.WriteLine($"user {username} unregistered successfully");
+                    }
+                }
+                else if (command == "register" && input.Length < 3)
+                {
+                    continue;
+                }
+                else if (command == "register")
+                {
+                    var licensePlate = input[2].ToCharArray();
+                    var licensePlateAsString = string.Empty;
+
+                    for (int j = 0; j < licensePlate.Length; j++)
+                    {
+                        licensePlateAsString += licensePlate[j];
+                    }
+
+                    if (licensePlate.Length != 8)
+                    {
+                        Console.WriteLine($"ERROR: invalid license plate {licensePlateAsString}");
+                        continue;
+                    }
+
+                    var isLetter = Char.IsLetter(licensePlate[0]) && Char.IsLetter(licensePlate[1]) && Char.IsLetter(licensePlate[6])
+                        && Char.IsLetter(licensePlate[7]);
+                    var isDigit = Char.IsDigit(licensePlate[2]) && Char.IsDigit(licensePlate[3]) && Char.IsDigit(licensePlate[4]) &&
+                        Char.IsDigit(licensePlate[5]);
+                    var licenseIsValid = false;
+
+                    if (isLetter && isDigit && licensePlate.Length == 8)
+                    {
+                        var isUpper = Char.IsUpper(licensePlate[0]) && Char.IsUpper(licensePlate[1]) && Char.IsUpper(licensePlate[6]) &&
+                            Char.IsUpper(licensePlate[7]);
+                        if (isUpper)
+                        {
+                            licenseIsValid = true;
+                        }
+                    }
+
+                    if (parking.ContainsKey(username))
+                    {
+                        Console.WriteLine($"ERROR: already registered with plate number {parking[username]}");
+                    }
+                    else if (!parking.ContainsKey(username))
+                    {
+                        if (!licenseIsValid)
+                        {
+                            Console.WriteLine($"ERROR: invalid license plate {licensePlateAsString}");
+                            continue;
+                        }
+                        if (parking.ContainsValue(licensePlateAsString))
+                        {
+                            Console.WriteLine($"ERROR: license plate {licensePlateAsString} is busy");
+                            continue;
+                        }
+                        parking.Add(username, licensePlateAsString);
+                        Console.WriteLine($"{username} registered {licensePlateAsString} successfully");
+                    }
+                }
+            }
+
+            foreach (var print in parking)
+            {
+                Console.WriteLine($"{print.Key} => {print.Value}");
+            }
+        }
+    }
+}
---
+++ dictionaries-lambda-and-linq/DictionariesListsMoreExercises/SupermarketDatabase/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SupermarketDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var products = new Dictionary<string, List<double>>();
+            var current = Console.ReadLine().Split().ToList();
+
+            while (current[0] != "stocked")
+            {
+                var name = current[0];
+                var price = double.Parse(current[1]);
+                var quantity = int.Parse(current[2]);
+
+                if (!products.ContainsKey(name))
+                {
+                    products.Add(name, new List<double>());
+                    products[name].Add(0);
+                    products[name].Add(0);
+                }
+                products[name][0] = price;
+                products[name][1] += quantity;
+
+                current = Console.ReadLine().Split().ToList();
+            }
+
+            double totalPrice = 0;
+
+            foreach (var product in products)
+            {
+                var price = product.Value[0] * product.Value[1];
+                Console.WriteLine($"{product.Key}: ${product.Value[0]:f2} * {product.Value[1]} = ${price:f2}");
+                totalPrice += price;
+            }
+
+            Console.WriteLine("{0}", string.Concat(Enumerable.Repeat("-", 30)));
+            Console.WriteLine($"Grand Total: ${totalPrice:f2}");
+        }
+    }
+}

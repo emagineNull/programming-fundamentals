bcf81a46ea621bfb7b47920b56d973fc32cb21ab
star-lord <alekrifai97@gmail.com>
2019-05-26 15:17:08 +0300

    Associative Arrays, Lambda Functions and LINQ

---
+++ dictionaries-lambda-and-linq/DictionariesListsMoreExercises/ByteFlip/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ByteFlip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ').Where(x => x.Length == 2).Reverse().ToList();
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                string reversedNum = new string(input[i].ToCharArray().Reverse().ToArray());
+                input[i] = reversedNum;
+            }
+            
+            for (int i = 0; i < input.Count; i++)
+            {
+                var hex = input[i];
+                var value = Convert.ToInt32(hex, 16);
+
+                Console.Write($"{(char)value}");
+            }
+        }
+    }
+}
---
+++ dictionaries-lambda-and-linq/DictionariesListsMoreExercises/TakeSkipRope/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TakeSkipRope
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().ToCharArray();
+
+            var numbers = new List<int>();
+            var nonNumbers = new List<char>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i].ToString().All(char.IsDigit))
+                {
+                    numbers.Add(int.Parse(input[i].ToString()));
+                }
+                else
+                {
+                    nonNumbers.Add(input[i]);
+                }
+            }
+
+            var take = new List<int>();
+            var skip = new List<int>();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    take.Add(numbers[i]);
+                }
+                else
+                {
+                    skip.Add(numbers[i]);
+                }
+            }
+
+            var skipCounter = 0;
+
+            for (int i = 0; i < take.Count; i++)
+            {
+                var result = nonNumbers.Skip(skipCounter).Take(take[i]).ToList();
+
+                Console.Write(string.Join("", result));
+                skipCounter += take[i] + skip[i];
+            }
+
+            Console.WriteLine();
+        }
+    }
+}

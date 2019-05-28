db72935c86a82468b96287dcbe2a55c892067e56
star-lord <alekrifai97@gmail.com>
2019-05-28 22:42:37 +0300

    Using Objects and Classes, Defining Simple Classes

---
+++ objects-and-classes/ObjectsAndClassesExercise/AdvertisementMessage/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AdvertisementMessage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var phrases = new string[] { "Excellent product.", "Such a great product.", "I always use that product.",
+            "Best product of its category.", "Exceptional product.", "I can’t live without this product." };
+
+            var events = new string[] { "Now I feel good.", "I have succeeded with this product.",
+            "Makes miracles. I am happy of the results!", "I cannot believe but now I feel awesome.",
+            "Try it yourself, I am very satisfied.", "I feel great!" };
+
+            var authors = new string[] { "Diana", "Petya", "Stella", "Elena", "Katya", "Iva", "Annie", "Eva" };
+            var cities = new string[] { "Burgas", "Sofia", "Plovdiv", "Varna", "Ruse" };
+
+            for (int i = 0; i < n; i++)
+            {
+                var rand = new Random();
+                var randomPhrase = phrases[rand.Next(phrases.Length)];
+                var randomEvent = events[rand.Next(events.Length)];
+                var randomAuthor = authors[rand.Next(authors.Length)];
+                var randomCity = cities[rand.Next(cities.Length)];
+
+                Console.WriteLine($"{randomPhrase} {randomEvent} {randomAuthor} - {randomCity}.");
+            }
+        }
+    }
+}
+

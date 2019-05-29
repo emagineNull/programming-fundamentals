c59e830874dd1537a99374d3b8071b48150c27fa
star-lord <alekrifai97@gmail.com>
2019-05-29 18:25:55 +0300

    Using Objects and Classes, Defining Simple Classes

---
+++ objects-and-classes/ObjectsAndClassesExercise/AverageGrades/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AverageGrades
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public List<double> Grades { get; set; }
+
+        public double AverageGrade
+        {
+            get
+            {
+                return Grades.Average();
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            Student[] students = GetStudents(n);
+            students = SortStudents(students);
+
+            PrintStudentsNameAvgGrade(students);
+        }
+
+        private static void PrintStudentsNameAvgGrade(Student[] students)
+        {
+            foreach (var student in students)
+            {
+                Console.WriteLine($"{student.Name} -> {student.AverageGrade:f2}");
+            }
+        }
+
+        private static Student[] SortStudents(Student[] students)
+        {
+            return students
+                .Where(ag => ag.AverageGrade >= 5.00)
+                .OrderBy(n => n.Name)
+                .ThenByDescending(ag => ag.AverageGrade)
+                .ToArray();
+        }
+
+        static Student[] GetStudents(int studentsCount)
+        {
+            var students = new Student[studentsCount];
+            for (int i = 0; i < studentsCount; i++)
+            {
+                var studentInfoList = Console.ReadLine()
+                    .Split(' ')
+                    .ToList();
+
+                students[i] = new Student
+                {
+                    Name = studentInfoList.First().ToString(),
+                    Grades = studentInfoList.Skip(1).Select(double.Parse).ToList()
+                };
+            }
+
+            return students;
+        }
+    }
+}
---
+++ objects-and-classes/ObjectsAndClassesExercise/BookLibrary/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace BookLibrary
+{
+    class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string ISBN { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    class Library
+    {
+        public string Name { get; set; }
+        public List<Book> BooksList = new List<Book>();
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            Library library = new Library();
+            List<Book> books = new List<Book>();
+            for (int i = 0; i < n; i++)
+            {
+                Book book = ReadBook();
+                books.Add(book);
+            }
+            library.BooksList = books;
+            PrintBooks(library);
+        }
+
+        private static void PrintBooks(Library library)
+        {
+            var ordered = library.BooksList.GroupBy(x => x.Author)
+                        .Select(g => new
+                        {
+                            Author = g.Key,
+                            Prices = g.Sum(s => s.Price)
+                        })
+                        .OrderByDescending(x => x.Prices)
+                        .ThenBy(x => x.Author)
+                        .ToList();
+
+            foreach (var author in ordered)
+            {
+                Console.WriteLine($"{author.Author} -> {author.Prices:f2}");
+            }
+        }
+        private static Book ReadBook()
+        {
+            var bookInfo = Console.ReadLine().Split(' ').ToArray();
+            Book book = new Book
+            {
+                Title = bookInfo[0],
+                Author = bookInfo[1],
+                Publisher = bookInfo[2],
+                ReleaseDate = DateTime.ParseExact(bookInfo[3], "dd.MM.yyyy", CultureInfo.InvariantCulture),
+                ISBN = bookInfo[4],
+                Price = decimal.Parse(bookInfo[5])
+            };
+
+            return book;
+        }
+    }
+}

6a1d06ccdcbdd91e7bce7f8fb36d198332e9bada
star-lord <alekrifai97@gmail.com>
2019-05-30 22:31:55 +0300

    Using Objects and Classes, Defining Simple Classes

---
+++ objects-and-classes/ObjectsAndClassesExercise/BookLibraryModification/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace BookLibraryModification
+{
+    class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string ISBN { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    class Library
+    {
+        public string Name { get; set; }
+        public List<Book> BooksList = new List<Book>();
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            Library library = new Library();
+            List<Book> books = new List<Book>();
+            for (int i = 0; i < n; i++)
+            {
+                Book book = ReadBook();
+                books.Add(book);
+            }
+            library.BooksList = books;
+            DateTime targetDate = DateTime.ParseExact(Console.ReadLine(), "dd.MM.yyyy", CultureInfo.InvariantCulture);
+            var bookReleasedAfter = GetBooksReleasedAfterDate(library, targetDate);
+            PrintTotalBooksPriveByAuthor(bookReleasedAfter);
+        }
+
+        static void PrintTotalBooksPriveByAuthor(Dictionary<string, DateTime> booksReleasedAfterDate)
+        {
+            foreach (var date in booksReleasedAfterDate.OrderBy(d => d.Value).ThenBy(t => t.Key))
+            {
+                Console.WriteLine($"{date.Key} -> {date.Value:dd.MM.yyyy}");
+            }
+        }
+
+        private static Dictionary<string, DateTime> GetBooksReleasedAfterDate(Library libraryOfBooks, DateTime targetDate)
+        {
+            var totalBooksPriveByAuthor = new Dictionary<string, DateTime>();
+            foreach (var book in libraryOfBooks.BooksList)
+            {
+                if (targetDate < book.ReleaseDate)
+                {
+
+                    totalBooksPriveByAuthor[book.Title] = book.ReleaseDate;
+
+
+                }
+            }
+
+            return totalBooksPriveByAuthor;
+        }
+
+        private static Book ReadBook()
+        {
+            var bookInfo = Console.ReadLine().Split(' ').ToArray();
+            Book book = new Book
+            {
+                Title = bookInfo[0],
+                Author = bookInfo[1],
+                Publisher = bookInfo[2],
+                ReleaseDate = DateTime.ParseExact(bookInfo[3], "dd.MM.yyyy", CultureInfo.InvariantCulture),
+                ISBN = bookInfo[4],
+                Price = decimal.Parse(bookInfo[5])
+            };
+
+            return book;
+        }
+    }
+}
---
+++ objects-and-classes/ObjectsAndClassesExercise/MentorGroup/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace MentorGroup
+{
+    class Group
+    {
+        public string Name { get; set; }
+        public List<DateTime> Dates = new List<DateTime>();
+        public List<string> Comments = new List<string>();
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(new char[] { ' ', ',' }).ToArray();
+            var students = new List<Group>();
+            var comments = new List<string>();
+            var dates = new List<DateTime>();
+
+            while (input[0] != "end")
+            {
+                if (input.Length > 1)
+                {
+                    dates = GetDate(input);
+                }
+
+                if (students.Any(x => x.Name == input[0]))
+                {
+                    students.Single(x => x.Name == input[0]).Dates.AddRange(dates);
+                }
+                else
+                {
+                    students.Add(NameAndDate(input, dates));
+                }
+
+                input = Console.ReadLine().Split(new char[] { ' ', ',' }).ToArray();
+            }
+
+            var commentsInput = Console.ReadLine().Split('-').ToArray();
+
+            while (commentsInput[0] != "end of comments")
+            {
+                GetComments(commentsInput, comments);
+                CommentsToGroup(students, commentsInput, comments);
+
+                comments.Clear();
+
+                commentsInput = Console.ReadLine().Split('-').ToArray();
+            }
+
+            PrintGroupMembers(students);
+        }
+
+        public static List<DateTime> GetDate(string[] input)
+        {
+            var currentDates = new List<DateTime>();
+            for (int i = 1; i < input.Length; i++)
+            {
+                currentDates.Add(DateTime.ParseExact(input[i], "dd/MM/yyyy", CultureInfo.InvariantCulture));
+            }
+
+            return currentDates;
+        }
+
+        public static Group NameAndDate(string[] input, List<DateTime> dates)
+        {
+            var student = new Group();
+
+            student.Name = input[0];
+            student.Dates = dates;
+
+            return student;
+        }
+
+        public static List<string> GetComments(string[] input, List<string> comments)
+        {
+            for (int i = 1; i < input.Length; i++)
+            {
+                comments.Add(input[i]);
+            }
+
+            return comments;
+        }
+
+        public static List<Group> CommentsToGroup(List<Group> students, string[] input, List<string> comments)
+        {
+            int indexa = students.FindIndex(r => r.Name == input[0]);
+
+            if (indexa < 0)
+            {
+                return students;
+            }
+
+            for (int i = 0; i < comments.Count; i++)
+            {
+                students[indexa].Comments.Add(comments[i]);
+            }
+
+            return students;
+        }
+
+        public static void PrintGroupMembers(List<Group> students)
+        {
+            foreach (var name in students.OrderBy(x => x.Name))
+            {
+                Console.WriteLine(name.Name);
+                Console.WriteLine("Comments:");
+                foreach (var comments in name.Comments)
+                {
+                    Console.WriteLine($"- {comments}");
+                }
+                Console.WriteLine("Dates attended:");
+                foreach (var dates in name.Dates.OrderBy(d => d.Date))
+                {
+                    Console.WriteLine($"-- {dates:dd/MM/yyyy}");
+                }
+            }
+        }
+    }
+}

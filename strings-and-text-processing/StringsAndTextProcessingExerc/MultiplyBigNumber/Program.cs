c5f9678d338783293490dc9c5b6276ea12836806
star-lord <alekrifai97@gmail.com>
2019-07-15 22:41:10 +0300

    Processing and Manipuling Text Using the .NET String Class

---
+++ strings-and-text-processing/StringsAndTextProcessingExerc/MultiplyBigNumber/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MultiplyBigNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num1 = Console.ReadLine().TrimStart('0');
+            var num2 = int.Parse(Console.ReadLine());
+
+            if (num2 == 0)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+
+            var decimalReminder = 0;
+            var currentMultiplication = 0;
+            var result = new List<int>();
+
+            for (int i = num1.Length - 1; i >= 0; i--)
+            {
+                var currentDigit = num1[i] - '0';
+                currentMultiplication = currentDigit * num2;
+                currentMultiplication += decimalReminder;
+
+                result.Add(currentMultiplication % 10);
+
+                decimalReminder = currentMultiplication / 10;
+            }
+
+            if (decimalReminder > 0)
+            {
+                result.Add(decimalReminder);
+            }
+
+            result.Reverse();
+
+            Console.WriteLine(string.Join("", result));
+        }
+    }
+}
--- strings-and-text-processing/StringsAndTextProcessingExerc/SumBigNumbers/Program.cs
+++ strings-and-text-processing/StringsAndTextProcessingExerc/SumBigNumbers/Program.cs
@@ -7,15 +7,16 @@ namespace SumBigNumbers
     {
         static void Main(string[] args)
         {
-            var input = Console.ReadLine().Split(' ');
-            var firstNumber = input[0];
-            var secondNumber = input[1];
+            var firstNumber = Console.ReadLine();
+            var secondNumber = Console.ReadLine();
 
             Console.WriteLine(Sum(firstNumber, secondNumber));
         }
 
         public static string Sum(string str1, string str2)
         {
+            str1 = str1.TrimStart('0');
+            str2 = str2.TrimStart('0');
             var length = str1.Length > str2.Length ? str1.Length : str2.Length;
             var firstNumList = new List<int>();
             var secondNumList = new List<int>();
@@ -28,7 +29,7 @@ namespace SumBigNumbers
                 }
                 catch
                 {
-                    firstNumList.Add(0);
+                    firstNumList.Insert(0, 0);
                 }
 
                 try
@@ -37,30 +38,48 @@ namespace SumBigNumbers
                 }
                 catch
                 {
-                    secondNumList.Add(0);
+                    secondNumList.Insert(0, 0);
                 }
             }
 
-            if (str1.Length < str2.Length)
-            {
-                firstNumList.Reverse();
-            }
-            else if (str1.Length > str2.Length)
-            {
-                secondNumList.Reverse();
-            }
+            //if (str1.Length < str2.Length)
+            //{
+            //    firstNumList.Reverse();
+            //}
+            //else if (str1.Length > str2.Length)
+            //{
+            //    secondNumList.Reverse();
+            //}
 
             var result = new List<string>();
+            var currentDigitsSum = 0;
             var temp = 0;
+            //var shorterLength = str1.Length > str2.Length ? str2.Length : str1.Length;
+            //var index = length - 1;
+
 
             for (int i = length - 1; i >= 0; i--)
             {
-                var currentDigitsSum = firstNumList[i] + secondNumList[i];
-                var currentDigitsSumAsText = string.Empty;
-                if (temp == 1)
-                {
-                    currentDigitsSum++;
-                }
+                //try
+                //{
+                    currentDigitsSum = firstNumList[i] + secondNumList[i] + temp;
+                //}
+                //catch
+                //{
+                //    if (str1.Length > str2.Length && index >= 0)
+                //    {
+                //        currentDigitsSum = firstNumList[index] + temp;
+                //    }
+                //    else if (str1.Length < str2.Length && index >= 0)
+                //    {
+                //        currentDigitsSum = secondNumList[index] + temp;
+                //    }
+                //    else
+                //    {
+                //        currentDigitsSum += temp;
+                //    }
+                //}
+
                 if (currentDigitsSum > 9)
                 {
                     temp = 1;
@@ -73,8 +92,13 @@ namespace SumBigNumbers
 
                 result.Add(currentDigitsSum.ToString());
             }
-            
 
+            if (temp == 1)
+            {
+                result.Add("1");
+            }
+
+            result.Reverse();
 
             return string.Join("", result);
         }

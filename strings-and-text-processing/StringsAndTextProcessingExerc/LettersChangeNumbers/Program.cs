c127d3a746408305c7c189b59bff6aa6b7348891
star-lord <alekrifai97@gmail.com>
2019-07-17 20:47:54 +0300

    Processing and Manipulating Text Using the .NET String Class

---
+++ strings-and-text-processing/StringsAndTextProcessingExerc/LettersChangeNumbers/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace LettersChangeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+
+            Console.WriteLine($"{Sum(input):f2}");
+        }
+
+        public static double Sum(string[] input)
+        {
+            var total = 0.0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                var currentText = input[i];
+                var currentNum = double.Parse(currentText.Substring(1, currentText.Length - 2));
+
+                var firstLetter = currentText[0];
+                var lastLetter = currentText[currentText.Length - 1];
+
+                var upperCaseFirst = firstLetter - 64.0;
+                var lowerCaseFirst = firstLetter - 96.0;
+                var upperCaseLast = lastLetter - 64.0;
+                var lowerCaseLast = lastLetter - 96.0;
+
+                if (firstLetter >= 65 && firstLetter <= 90)
+                {
+                    total += currentNum / upperCaseFirst;
+                }
+                else
+                {
+                    total += currentNum * lowerCaseFirst;
+                }
+
+                if (lastLetter >= 65 && lastLetter <= 90)
+                {
+                    total -= (lastLetter - 64);
+                }
+                else
+                {
+                    total += (lastLetter - 96);
+                }
+            }
+
+            return total;
+        }
+    }
+}
---
+++ strings-and-text-processing/StringsAndTextProcessingExerc/MelrahShake/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MelrahShake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = Console.ReadLine();
+            var pattern = Console.ReadLine();
+
+            Console.WriteLine(Shaker(str, pattern));
+        }
+
+        public static string Shaker(string str, string pattern)
+        {
+            while (str.Length > 0 && pattern.Length > 0)
+            {
+                var firstIndex = str.IndexOf(pattern);
+                var lastIndex = str.LastIndexOf(pattern);
+
+                if (str.Contains(pattern) && firstIndex >= 0 && lastIndex >= 0 && firstIndex != lastIndex)
+                {
+                    str = str.Remove(firstIndex, pattern.Length);
+                    lastIndex = str.LastIndexOf(pattern);
+                    str = str.Remove(lastIndex, pattern.Length);
+
+                    pattern = pattern.Remove(pattern.Length / 2, 1);
+
+                    Console.WriteLine("Shaked it.");
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine("No shake.");
+            return str;
+        }
+    }
+}

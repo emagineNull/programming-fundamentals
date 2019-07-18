4b1c6141bb2a62dcf9da9e3307ae0f2311f22851
star-lord <alekrifai97@gmail.com>
2019-07-18 23:55:41 +0300

    Regular Expressions Language Syntax Advanced Text Processing with RegEx

---
+++ regular-expressions-regex/Regex/MatchFullName/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace MatchFullName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var regex = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+
+            var names = Console.ReadLine();
+
+            MatchCollection matchedNames = Regex.Matches(names, regex);
+
+            foreach (Match name in matchedNames)
+            {
+                Console.Write($"{name.Value} ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ regular-expressions-regex/Regex/MatchPhoneNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace MatchPhoneNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var regex = new Regex(@"\+359( |\-)2\1\d{3}\1\d{4}\b");
+
+            var numbers = Console.ReadLine();
+
+            var matchedPhones = regex.Matches(numbers);
+
+            var phonesMatched = matchedPhones
+                .Cast<Match>()
+                .Select(a => a.Value.Trim())
+                .ToArray();
+
+            Console.WriteLine(string.Join(", ", phonesMatched));
+        }
+    }
+}

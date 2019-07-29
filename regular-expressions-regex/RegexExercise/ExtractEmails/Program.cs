421fdd5f0ea1c95068c447755b6f85f1782558a9
star-lord <alekrifai97@gmail.com>
2019-07-29 21:50:53 +0300

    Regular Expressions Language Syntax Advanced Text Processing with RegEx

---
+++ regular-expressions-regex/RegexExercise/ExtractEmails/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace ExtractEmails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            
+            var regex = new Regex(@"(^|(?<=\s))[A-Za-z]+([0-9]+)?([\._-][A-z0-9]+)?@\w+-?\w+(?:\.\w+)+");
+
+            var emails = regex.Matches(input);
+
+            var emailMatches = emails.Cast<Match>().Select(a => a.Value).ToArray();
+
+            foreach (var email in emailMatches)
+            {
+                Console.WriteLine(email);
+            }
+        }
+    }
+}
---
+++ regular-expressions-regex/RegexExercise/ExtractSentencesByKeyword/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ExtractSentencesByKeyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var word = Console.ReadLine();
+            var text = Console.ReadLine();
+
+            var regex = new Regex(@"(^|(?<=\s))[A-Z][^.!?]*\W" + word + @"\W.*?(?=[\.\?\!])");
+
+            var matchedSentences = regex.Matches(text);
+
+            foreach (Match sentence in matchedSentences)
+            {
+                Console.WriteLine(sentence.Value);
+            }
+        }
+    }
+}

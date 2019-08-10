3c0026ca262e85c94807c209ec37b137d2dd02fe
star-lord <alekrifai97@gmail.com>
2019-08-10 23:51:28 +0300

    Exam Preparation

---
+++ exam-preparation/ExamPrepIII/AnonymousDownsite/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AnonymousDownsite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var securityKey = int.Parse(Console.ReadLine());
+
+            var input = new string[3];
+            var websites = new List<string>();
+            var totalLoss = 0.0M;
+            var length = n;
+
+            for (int i = 0; i < length; i++)
+            {
+                input = Console.ReadLine().Split(' ');
+
+                totalLoss += decimal.Parse(input[1]) * decimal.Parse(input[2]);
+
+                websites.Add(input[0]);
+            }
+            
+            var securityToken = Math.Pow(securityKey, n);
+
+            foreach (var sites in websites)
+            {
+                Console.WriteLine(sites);
+            }
+
+            Console.WriteLine($"Total Loss: {totalLoss:f20}");
+
+            Console.WriteLine($"Security Token: {securityToken}");
+        }
+    }
+}
---
+++ exam-preparation/ExamPrepIII/AnonymousThreat/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace AnonymousThreat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = Console.ReadLine().Split(' ').ToList();
+
+            var commands = Console.ReadLine();
+
+            while (commands != "3:1")
+            {
+                var currentCommand = commands.Split(' ');
+
+                var command = currentCommand[0];
+
+                if (command == "merge")
+                {
+                    var startIndex = int.Parse(currentCommand[1]);
+                    var endIndex = int.Parse(currentCommand[2]);
+
+                    if ((startIndex >= 0 || startIndex < data.Count) && (endIndex >= 0 || endIndex < data.Count))
+                    {
+                        var concat = string.Concat(data[startIndex], data[endIndex]);
+
+                        data[startIndex] = concat;
+                        data.RemoveAt(endIndex);
+                    }
+                }
+                if (command == "divide")
+                {
+                    var index = int.Parse(currentCommand[1]);
+                    var partitions = int.Parse(currentCommand[2]);
+                    var divided = new List<string>();
+
+                    if (data[index].Length % partitions == 0)
+                    {
+                        divided = Enumerable.Range(0, data[index].Length / partitions)
+                                        .Select(i => data[index].Substring(i * partitions, partitions)).ToList();
+                    }
+
+                    data.RemoveAt(index);
+
+                    for (int i = index; i < divided.Count; i++)
+                    {
+                        var dividedIndexes = divided.Count - 1;
+
+                        data.AddRange(divided);
+                    }
+                }
+
+                commands = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(" ", data));
+        }
+    }
+}

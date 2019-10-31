50282e456739e1348300cea232d1a2454f2a6ff3
star-lord <alekrifai97@gmail.com>
2019-10-31 20:23:34 +0200

    Mid Exam Preparation

---
+++ exam-preparation/MidExamPreparation/ManOWar/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ManOWar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] piratesInput = Console.ReadLine().Split('>').Select(int.Parse).ToArray();
+            int[] warShipInput = Console.ReadLine().Split('>').Select(int.Parse).ToArray();
+            int maxHealth = int.Parse(Console.ReadLine());
+
+            var pirateShip = new List<int>(piratesInput);
+            var warShip = new List<int>(warShipInput);
+
+            string command = Console.ReadLine();
+
+            while (command != "Retire")
+            {
+                string[] commandParts = command.Split();
+
+                string action = commandParts[0];
+
+                if (action == "Fire")
+                {
+                    int index = int.Parse(commandParts[1]);
+
+                    if (index >= 0 && index < warShip.Count)
+                    {
+                        int damage = int.Parse(commandParts[2]);
+
+                        if (warShip[index] - damage <= 0)
+                        {
+                            Console.WriteLine("You won! The enemy ship has sunken.");
+                            return;
+                        }
+
+                        warShip[index] -= damage;
+                    }
+                }
+                else if (action == "Defend")
+                {
+                    int startIndex = int.Parse(commandParts[1]);
+                    int endIndex = int.Parse(commandParts[2]);
+
+                    if (startIndex >= 0 && startIndex < pirateShip.Count &&
+                        endIndex >= 0 && endIndex < pirateShip.Count && startIndex < endIndex)
+                    {
+                        int damage = int.Parse(commandParts[3]);
+                        for (int i = startIndex; i <= endIndex; i++)
+                        {
+                            if (pirateShip[i] - damage <= 0)
+                            {
+                                Console.WriteLine("You lost! The pirate ship has sunken.");
+                                return;
+                            }
+
+                            pirateShip[i] -= damage;
+                        }
+                    }
+                }
+                else if (action == "Repair")
+                {
+                    int index = int.Parse(commandParts[1]);
+
+                    if (index >= 0 && index < pirateShip.Count)
+                    {
+                        int health = int.Parse(commandParts[2]);
+
+                        if (pirateShip[index] + health > maxHealth)
+                        {
+                            pirateShip[index] = maxHealth;
+                        }
+                        else
+                        {
+                            pirateShip[index] += health;
+                        }
+                    }
+                }
+                else if (action == "Status")
+                {
+                    int counter = 0;
+                    double needRepair = maxHealth * 0.2;
+
+                    for (int i = 0; i < pirateShip.Count; i++)
+                    {
+                        if (pirateShip[i] < needRepair)
+                        {
+                            counter++;
+                        }
+                    }
+
+                    Console.WriteLine($"{counter} sections need repair.");
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Pirate ship status: {pirateShip.Sum()}");
+            Console.WriteLine($"Warship status: {warShip.Sum()}");
+        }
+    }
+}
---
+++ exam-preparation/MidExamPreparation/TreasureHunt/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TreasureHunt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] initialLoot = Console.ReadLine().Split('|');
+
+            var loot = new List<string>(initialLoot);
+
+            string command = Console.ReadLine();
+
+            while (command != "Yohoho!")
+            {
+                string[] commandParts = command.Split();
+
+                string action = commandParts[0];
+
+                if (action == "Loot")
+                {
+                    for (int i = 1; i < commandParts.Length; i++)
+                    {
+                        if (!loot.Contains(commandParts[i]))
+                        {
+                            loot.Insert(0, commandParts[i]);
+                        }
+                    }
+                }
+                else if (action == "Drop")
+                {
+                    int index = int.Parse(commandParts[1]);
+
+                    if (index >= 0 && index < loot.Count)
+                    {
+                        string removed = loot[index];
+                        loot.RemoveAt(index);
+                        loot.Add(removed);
+                    }
+                }
+                else if (action == "Steal")
+                {
+                    int count = int.Parse(commandParts[1]);
+
+                    var stolenLoot = new List<string>();
+
+                    if (count > loot.Count)
+                    {
+                        stolenLoot = loot;
+                        Console.WriteLine(string.Join(", ", stolenLoot));
+                        loot.Clear();
+                    }
+                    else
+                    {
+                        for (int i = loot.Count - count; i < loot.Count; i++)
+                        {
+                            stolenLoot.Add(loot[i]);
+                        }
+
+                        loot.RemoveRange(loot.Count - count, count);
+                        Console.WriteLine(string.Join(", ", stolenLoot));
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+
+            if (loot.Any())
+            {
+                double sum = 0.0;
+                foreach (var item in loot)
+                {
+                    sum += item.Length;
+                }
+
+                Console.WriteLine($"Average treasure gain: {sum / loot.Count:f2} pirate credits.");
+            }
+            else
+            {
+                Console.WriteLine("Failed treasure hunt.");
+            }
+        }
+    }
+}

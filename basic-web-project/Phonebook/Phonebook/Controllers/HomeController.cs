810abf444ee8b8d104f9ade927e37d2bb58900fb
star-lord <alekrifai97@gmail.com>
2019-12-10 17:55:42 +0200

    C# Fundamentals - Basic Web Project - September 2019

---
+++ basic-web-project/Phonebook/Phonebook/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Phonebook.Data;
+using Phonebook.Data.Models;
+
+namespace Phonebook.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            // get a list of contacts from the data access method
+            var contactsList = DataAccess.Contacts;
+
+            // contactsList.Add(new Contact() { Name = "Alek Rifai", Number = "0888888888"});
+
+            // pass the list of contacts to the View
+            return View(contactsList);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Contact incomingContact)
+        {
+            // Add the incoming contact to the contacts list.
+            DataAccess.Contacts.Add(incomingContact);
+
+            // pass the list of contacts to the View
+            return View("Index", DataAccess.Contacts);
+        }
+    }
+}
---
+++ basic-web-project/Phonebook/Phonebook/Data/DataAccess.cs
@@ -0,0 +1,15 @@
+﻿using Phonebook.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Phonebook.Data
+{
+    // Responsible for providing a list of contacts
+
+    public class DataAccess
+    {
+        public static List<Contact> Contacts { get; set; } = new List<Contact>();
+    }
+}
